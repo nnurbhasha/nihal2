f2b8ff9584b2cc7ab2086702a1b0d092cd9c21fe
Nihal Nurbhasha <Nihal@Nihals-MacBook-Pro.local>
2020-06-17 10:27:39 -0500

    Contact page setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,7 +7,29 @@ function AboutPage(props) {
   return (
     <div>
       <Hero title={props.title} />
-      <Content>Hello</Content>
+      <Content>
+        <p>
+          Hello, my name is Nihal. I am a software enginner with experience in
+          React, Node.js, Express, Python, SQL Server, PostgreSQL and MongoDB.
+        </p>
+        <p>
+          My dream is to push human advancement by utilizing data in it's many
+          forms.
+        </p>
+        <p>
+          I am constantly learning new things. Currently those thinkgs include
+          gaining more experience with React, Node.js, Express, PostgreSQL and
+          MongoDB.
+        </p>
+        <p>
+          My latest project, Money Manager, is a financual tool for all people.
+          You can check it out here, or on the homepage. It is build with React.
+        </p>
+        <p>
+          When I am not programming, chances are I'm wrting a blog post. You can
+          find those here.
+        </p>
+      </Content>
     </div>
   );
 }
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,104 @@
 import React from "react";
 
-// function component; does not have to manage state
-function ContactPage(props) {
-  return <p>ContactPage Works!</p>;
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import Hero from "../components/Hero";
+import Content from "../components/Content";
+
+// class based component; need to manage state
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disabled: false,
+      emailSent: null,
+    };
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    // prevents default behavior, which is to refresh
+    event.preventDefault();
+    // disable ability of someone to submit twice; service limits exist
+    this.setState({
+      disabled: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Hero title={this.props.title} />
+        <Content>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor="full-name">Full Name</Form.Label>
+              <Form.Control
+                id="full-name"
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control
+                id="email"
+                name="email"
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="message">Message</Form.Label>
+              <Form.Control
+                id="message"
+                name="message"
+                as="textarea"
+                rows="3"
+                value={this.state.message}
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Button
+              className="d-inline-block"
+              variant="primary"
+              type="submit"
+              disabled={this.state.disabled}
+            >
+              Send
+            </Button>
+
+            {this.state.emailSent === true && (
+              <p className="d-inline success-msg">Email Sent</p>
+            )}
+
+            {this.state.emailSent === false && (
+              <p className="d-inline err-msg">Email Not Sent</p>
+            )}
+          </Form>
+        </Content>
+      </div>
+    );
+  }
 }
 
 export default ContactPage;
